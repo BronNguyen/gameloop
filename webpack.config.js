376b1d978911c11479cd454c258278e3b940e899
thinh nguyen <thinh.nt93@gmail.com>
2021-05-10 18:27:45 +0700

    configued webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,30 @@ const path = require("path");
 
 module.exports = {
   mode: "development", // could be "production" as well
-  entry: "./src/index.js",
+  entry: './src/index.js',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "app.js",
   },
-  rules: [
-    { 
-      test: /\.css$/, 
-      use: ["style-loader", "css-loader"] 
-    },
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: "babel-loader",
-        options: { presets: ["@babel/preset-env"] },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       },
-    },
-  ],
-  plugins: [    new HTMLWebpackPlugin()  ] 
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ],
+  },
 };
