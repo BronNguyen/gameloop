2a5d4c699f51c101846b69097645bb29bfdf62aa
Tiến Thịnh Nguyễn <thinh.nt93@gmail.com>
2021-05-19 07:48:17 +0700

    update

--- src/SuperiorPhaser/InputManager.ts
+++ src/SuperiorPhaser/InputManager.ts
@@ -12,6 +12,8 @@ export default class InputManager {
     }
   });
 
+  
+
   releasedInput = window.addEventListener("keyup", (event) => {
     if (this.registerKeyPress(event.key)) {
       (this.currentlyDownKey = undefined), true;
@@ -24,10 +26,114 @@ export default class InputManager {
   },false)
 
   handle() {
-    this.queue.foreach(events => forEach(eventListener) {
-      listener()
-    }
+      document.addEventListener("keydown",this._onkeydown)
+    // this.queue.foreach(events => forEach(eventListener) {
+    //   listener()
+    // }
   }
+  
+  //inputmanager.getkeypressed(inputManager.down
+  
+  
+// class InputManager{
+    
+//     static instance = null;
+//     static getInstance(){
+//         if (InputManager.instance) return InputManager.instance;
+//         return new InputManager();
+//     }
+//     constructor(){ 
+//         if (InputManager.instance) return InputManager.instance;
+//         this.eventKeyboard = {
+//             keyDown: {},
+//             keyUp: {},
+//             keyPress: {}
+//         };
+//         this.keyCode = {
+//             BACKSPACE: 8,
+//             COMMA: 188,
+//             DELETE: 46,
+//             DOWN: 40,
+//             END: 35,
+//             ENTER: 13,
+//             ESCAPE: 27,
+//             HOME: 36,
+//             LEFT: 37,
+//             NUMPAD_ADD: 107,
+//             NUMPAD_DECIMAL: 110,
+//             NUMPAD_DIVIDE: 111,
+//             NUMPAD_ENTER: 108,
+//             NUMPAD_MULTIPLY: 106,
+//             NUMPAD_SUBTRACT: 109,
+//             PAGE_DOWN: 34,
+//             PAGE_UP: 33,
+//             PERIOD: 190,
+//             RIGHT: 39,
+//             SPACE: 32,
+//             TAB: 9,
+//             UP: 38
+//         };
+        
+
+//         InputManager.instance = this;
+//     }
+//     start(){
+//         document.addEventListener("keydown",this._onKeyDown(this));
+//         document.addEventListener("keyup",this._onKeyUp(this));
+//     }
+
+//     getKeyDown(keyCode){
+//         if (this.eventKeyboard.keyDown[keyCode] == 1){            
+//             return true;
+//         } 
+//         return false
+//     }
+    
+
+//     getKeyUp(keyCode){
+//         if (this.eventKeyboard.keyUp[keyCode] == 1) return true;
+//         return false
+//     }
+
+//     getKeyPress(keyCode){
+//         if (this.eventKeyboard.keyPress[keyCode] == 1) return true;
+//         return false
+//     }
+
+//     clear(){
+//         this.eventKeyboard = {
+//             keyDown: {},
+//             keyUp: {},
+//             keyPress: {}
+//         }
+//     }
+
+//     default(){
+//         return {
+//             keyDown: {},
+//             keyUp: {},
+//             keyPress: {}
+//         }
+//     }
+
+//     _onKeyDown(obj){
+//         return (event) =>{
+//             if (!obj.getKeyPress(event.keyCode)){
+//                 obj.eventKeyboard.keyDown[event.keyCode] = 1
+//                 obj.eventKeyboard.keyPress[event.keyCode] = 1
+//             }
+//         }
+//     }
+//     _onKeyUp(obj){
+//         return (event) =>{
+//             this.eventKeyboard.keyUp[event.keyCode] = 1
+//             delete this.eventKeyboard.keyPress[event.keyCode]
+//         }
+//     }
+
+// }
+// let inputManager = new InputManager();
+// export default inputManager;)
 
   registerKeyPress(input: string) {
     if (
